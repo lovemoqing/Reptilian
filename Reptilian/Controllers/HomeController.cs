b94e7f44f7ac967a2ee0022cfd00b7cbab516abb
penglong <penglong@tidebuy.net>
2018-08-29 15:42:43 +0800

    爬取12306最新日期的硬卧车次信息

--- Reptilian/Controllers/HomeController.cs
+++ Reptilian/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ namespace Reptilian.Controllers
 
         public JsonResult GetKeys(string path)
         {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                string datetime = DateTime.Now.AddDays(29).ToString("yyyy-MM-dd");
+                path = string.Format("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={0}&leftTicketDTO.from_station=BJP&leftTicketDTO.to_station=WHN&purpose_codes=ADULT", datetime);
+            }
             string html = HttpHelper.GetHtml(path);
             List<string> list = new List<string>();
             if (!string.IsNullOrWhiteSpace(html))
@@ -31,7 +36,7 @@ namespace Reptilian.Controllers
                     list.Add(item.ToString().Replace("<span>", "").Replace("</span>", ""));
                 }
             }
-            return Json(html)
+            return Json(html);
         }
     }
 }
